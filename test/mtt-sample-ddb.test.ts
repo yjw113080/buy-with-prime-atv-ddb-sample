df42add4f9f5e5dbe3ff36610d342aa0265ee6e0
Jiwon Yeom <jiwony@amazon.com>
2023-09-08 15:53:10 -0700

    Initial commit

---
+++ test/mtt-sample-ddb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MttSampleDdb from '../lib/mtt-sample-ddb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mtt-sample-ddb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MttSampleDdb.MttSampleDdbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
